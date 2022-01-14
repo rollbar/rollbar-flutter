065dadb6b9133d892bd1477ac9c2009036446016
Andrey Kornich <akornich@gmail.com>
2022-01-13 19:35:15 -0800

    refactor: duplicated payload repository as async and sync versions

--- rollbar_dart/lib/src/payload_repository/payload_repository.dart
+++ rollbar_dart/lib/src/payload_repository/payload_repository.dart
@@ -10,43 +10,87 @@ class PayloadRepository {
 
   PayloadRepository(this._dataAccess);
 
-  static Future<PayloadRepository> create(bool persistent) async {
+  // Factory methods:
+  ///////////////////
+
+  static PayloadRepository create(bool persistent) {
     var dataAccess = DbDataAccess().initialize(asPersistent: persistent);
     dataAccess.deleteUnusedDestinations();
     return PayloadRepository(dataAccess);
   }
 
-  static Future<PayloadRepository> createInMemory() async {
+  static PayloadRepository createInMemory() {
     return create(false);
   }
 
-  static Future<PayloadRepository> createPersistent() async {
+  static PayloadRepository createPersistent() {
     return create(true);
   }
 
-  Future<Set<Destination>> getDestinations() async {
+  // Entities manipulation methods:
+  /////////////////////////////////
+
+  Set<Destination> getDestinations() {
     return _dataAccess.selectAllDestinations();
   }
 
-  Future<int> addDestination(Destination destination) async {
+  int addDestination(Destination destination) {
     final int id = _dataAccess.insertDestination(destination);
     return id;
   }
 
-  Future<Set<PayloadRecord>> getPayloadRecords() async {
+  Set<PayloadRecord> getPayloadRecords() {
     return _dataAccess.selectAllPayloadRecords();
   }
 
-  Future<Set<PayloadRecord>> getPayloadRecordsForDestination(
-      Destination destination) async {
+  Set<PayloadRecord> getPayloadRecordsForDestination(Destination destination) {
     if (destination.id != null) {
       return _dataAccess.selectPayloadRecordsWithDestinationID(destination.id!);
     }
     return <PayloadRecord>{};
   }
 
-  Future<Set<PayloadRecord>> getPayloadRecordsWithDestinationID(
-      int destinationID) async {
+  Set<PayloadRecord> getPayloadRecordsWithDestinationID(int destinationID) {
     return _dataAccess.selectPayloadRecordsWithDestinationID(destinationID);
+  }
+
+  // Async factory methods:
+  /////////////////////////
+
+  static Future<PayloadRepository> createAsync(bool persistent) async {
+    return create(persistent);
+  }
+
+  static Future<PayloadRepository> createInMemoryAsync() async {
+    return create(false);
+  }
+
+  static Future<PayloadRepository> createPersistentAsync() async {
+    return create(true);
+  }
+
+  // Async entities manipulation methods:
+  ///////////////////////////////////////
+
+  Future<Set<Destination>> getDestinationsAsync() async {
+    return getDestinations();
+  }
+
+  Future<int> addDestinationAsync(Destination destination) async {
+    return addDestination(destination);
+  }
+
+  Future<Set<PayloadRecord>> getPayloadRecordsAsync() async {
+    return getPayloadRecords();
+  }
+
+  Future<Set<PayloadRecord>> getPayloadRecordsForDestinationAsync(
+      Destination destination) async {
+    return getPayloadRecordsForDestination(destination);
+  }
+
+  Future<Set<PayloadRecord>> getPayloadRecordsWithDestinationIDAsync(
+      int destinationID) async {
+    return getPayloadRecordsWithDestinationID(destinationID);
   }
 }
