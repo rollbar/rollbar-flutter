7128502f8ad82fdca0d7d29d6bacb768947844ab
Matias Pequeno <matias.pequeno@gmail.com>
2022-07-17 14:58:57 -0300

    Destination is now an immutable data structure

--- rollbar_dart/lib/src/payload_repository/destination.dart
+++ rollbar_dart/lib/src/payload_repository/destination.dart
@@ -2,73 +2,71 @@ import 'dart:convert';
 
 import 'package:meta/meta.dart';
 
+import '../ext/collections.dart';
+
+@sealed
+@immutable
 class Destination {
-  int? _id;
-  late final String endpoint;
-  late final String accessToken;
+  final int id;
+  final String endpoint;
+  final String accessToken;
 
-  Destination.create({
-    required String endpoint,
-    required String accessToken,
-  }) : this(endpoint: endpoint, accessToken: accessToken);
+  // Destination.create({
+  //   required String endpoint,
+  //   required String accessToken,
+  // }) : this(endpoint: endpoint, accessToken: accessToken);
 
-  Destination({required this.endpoint, required this.accessToken, int? id})
-      : _id = id;
+  Destination({
+    required this.endpoint,
+    required this.accessToken,
+    required this.id,
+  });
 
-  int? get id => _id;
+  // int? get id => _id;
 
-  @protected
-  void assignID(int? value) {
-    _id = value;
-  }
+  // @protected
+  // void assignID(int? value) {
+  //   _id = value;
+  // }
 
   Destination copyWith({
     int? id,
     String? endpoint,
     String? accessToken,
-  }) {
-    return Destination(
-      endpoint: endpoint ?? this.endpoint,
-      accessToken: accessToken ?? this.accessToken,
-      id: id,
-    );
-  }
+  }) =>
+      Destination(
+          endpoint: endpoint ?? this.endpoint,
+          accessToken: accessToken ?? this.accessToken,
+          id: id ?? this.id);
 
-  Map<String, dynamic> toMap() {
-    return {
-      '_id': _id,
-      'endpoint': endpoint,
-      'accessToken': accessToken,
-    };
-  }
+  JsonMap toMap() => {
+        '_id': id,
+        'endpoint': endpoint,
+        'accessToken': accessToken,
+      };
 
-  factory Destination.fromMap(Map<String, dynamic> map) {
-    return Destination(
+  factory Destination.fromMap(Map<String, dynamic> map) => Destination(
       id: map['id']?.toInt(),
-      endpoint: map['endpoint'] ?? '',
-      accessToken: map['accessToken'] ?? '',
-    );
-  }
+      endpoint: map['endpoint'],
+      accessToken: map['accessToken']);
 
-  String toJson() => json.encode(toMap());
+  String toJson() => jsonEncode(toMap());
 
-  factory Destination.fromJson(String source) =>
-      Destination.fromMap(json.decode(source));
+  factory Destination.fromJson(String json) =>
+      Destination.fromMap(jsonDecode(json));
 
   @override
   String toString() =>
-      'Destination(_id: $_id, endpoint: $endpoint, accessToken: $accessToken)';
+      'Destination(_id: $id, endpoint: $endpoint, accessToken: $accessToken)';
 
   @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is Destination &&
-        other._id == _id &&
-        other.endpoint == endpoint &&
-        other.accessToken == accessToken;
-  }
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is Destination &&
+          other.id == id &&
+          other.endpoint == endpoint &&
+          other.accessToken == accessToken);
 
   @override
-  int get hashCode => _id.hashCode ^ endpoint.hashCode ^ accessToken.hashCode;
+  int get hashCode => id.hashCode ^ endpoint.hashCode ^ accessToken.hashCode;
 }
