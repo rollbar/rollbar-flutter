599bbce35c6d40a150efc2cab8fbb550f41762cd
Matias Pequeno <matias.pequeno@gmail.com>
2022-11-12 15:28:20 +0100

    Reimplemented internal Rollbar HTTP Response structure.
    squash: Reimplemented internal Rollbar HTTP Response structure.
    
    
    squash: Reimplemented internal Rollbar HTTP Response structure.

--- rollbar_common/lib/src/identifiable.dart
+++ rollbar_common/lib/src/identifiable.dart
@@ -7,6 +7,19 @@ const uuidGen = Uuid();
 
 typedef UUID = UuidValue;
 
+final nilUUID = UUID("00000000-0000-0000-0000-000000000000");
+
 abstract class Identifiable<T extends Object> {
   T get id;
+}
+
+extension IterableIntoUUID on Iterable<int> {
+  UUID toUUID() => UUID.fromList(toList());
+}
+
+extension StringIntoUUID on String {
+  UUID toUUID() => RegExp(r'\w\w')
+      .allMatches(this)
+      .map((match) => int.parse(match[0]!, radix: 16))
+      .toUUID();
 }
