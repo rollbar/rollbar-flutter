350e5641ed5f597ae45987ae76a6391ba00cbdbe
Matias Pequeno <matias.pequeno@gmail.com>
2022-11-14 11:44:52 +0100

    Improved error handling on HTTP Sender
    squash: Improved error handling on HTTP Sender
    
    
    squash: Improved error handling on HTTP Sender.
    
    
    sq: HTTP Sender
    
    
    sq: HTTP Sen

--- rollbar_common/lib/src/http.dart
+++ rollbar_common/lib/src/http.dart
@@ -1,7 +1,30 @@
+import 'package:http/http.dart' as http;
+
 enum HttpMethod { get, head, post, put, delete, connect, options, trace, patch }
+
+enum HttpStatus { info, success, redirect, clientError, serverError }
 
 typedef HttpHeaders = Map<String, String>;
 
 extension HttpMethodName on HttpMethod {
   String get name => toString().split('.').last.toUpperCase();
+}
+
+extension HttpResponseExtension on http.Response {
+  /// The HTTP status for this response derived from the status code.
+  HttpStatus get status {
+    if (statusCode >= 100 && statusCode < 200) {
+      return HttpStatus.info;
+    } else if (statusCode >= 200 && statusCode < 300) {
+      return HttpStatus.success;
+    } else if (statusCode >= 300 && statusCode < 400) {
+      return HttpStatus.redirect;
+    } else if (statusCode >= 400 && statusCode < 500) {
+      return HttpStatus.clientError;
+    } else if (statusCode >= 500 && statusCode < 600) {
+      return HttpStatus.serverError;
+    } else {
+      throw StateError('http status code $statusCode is invalid.');
+    }
+  }
 }
--- rollbar_dart/lib/src/sender/http_sender.dart
+++ rollbar_dart/lib/src/sender/http_sender.dart
@@ -3,33 +3,28 @@ import 'dart:developer';
 import 'dart:io';
 
 import 'package:meta/meta.dart';
-import 'package:http/http.dart' as http;
 import 'package:rollbar_common/rollbar_common.dart';
 
-import '../data/response.dart';
+import '../config.dart';
 import 'sender.dart';
 
 /// HTTP [Sender] implementation.
 @sealed
 @immutable
 @internal
-class HttpSender implements Sender {
-  final Uri _endpoint;
-  final HttpHeaders _headers;
+class HttpSender implements Configurable, Sender {
+  @override
+  final Config config;
+  final HttpHeaders headers;
+  final Uri uri;
 
-  HttpSender({required String accessToken, required String endpoint})
-      : _endpoint = Uri.parse(endpoint),
-        _headers = {
+  HttpSender(this.config)
+      : uri = Uri.parse(config.endpoint),
+        headers = {
           'User-Agent': 'rollbar-dart',
           'Content-Type': 'application/json',
-          'X-Rollbar-Access-Token': accessToken,
+          'X-Rollbar-Access-Token': config.accessToken,
         };
-
-  static Future<bool> sendRecord(PayloadRecord record) async =>
-      await HttpSender(
-        endpoint: record.endpoint,
-        accessToken: record.accessToken,
-      ).sendString(record.payload);
 
   /// Sends the provided payload as the body of POST request to the configured
   /// endpoint.
@@ -38,50 +33,30 @@ class HttpSender implements Sender {
 
   @override
   Future<bool> sendString(String payload) async {
+    final client = config.httpClient();
+
     try {
-      if (_State.suspended) {
-        throw HttpException(
-          'HTTP transactions are currently suspended.',
-          uri: _endpoint,
-        );
-      }
+      final response = await client.post(uri, headers: headers, body: payload);
 
-      final response = await http
-          .post(_endpoint, headers: _headers, body: payload)
-          .then(Response.from);
-
-      if (response.isError) {
+      if (response.status != HttpStatus.success) {
         throw HttpException(
-          '${response.error}: ${response.message}',
-          uri: _endpoint,
+          response.reasonPhrase ?? response.status.name,
+          uri: uri,
         );
       }
 
       return true;
     } catch (error, stackTrace) {
-      if (!_State.suspended) _State.suspend(30.seconds);
-
-      log('Exception sending payload',
+      log('Error sending payload to \'$uri\'',
+          name: 'Rollbar.${runtimeType.toString()}',
           time: DateTime.now(),
           level: Level.error.value,
-          name: runtimeType.toString(),
           error: error,
           stackTrace: stackTrace);
 
       return false;
+    } finally {
+      client.close();
     }
-  }
-}
-
-extension _State on HttpSender {
-  static bool _suspended = false;
-
-  static bool get suspended => _suspended;
-
-  static void suspend(Duration duration) async {
-    if (_suspended) return;
-    _suspended = true;
-    await Future.delayed(duration);
-    _suspended = false;
   }
 }
