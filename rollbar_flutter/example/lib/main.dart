be5e66500e06247cc9aa21f87d2f2cb7f51bb711
Matias Pequeno <matias.pequeno@gmail.com>
2023-03-10 12:21:34 -0300

    Removed access token from example.
    accesstoken

--- rollbar_flutter/example/lib/main.dart
+++ rollbar_flutter/example/lib/main.dart
@@ -11,7 +11,7 @@ import 'package:rollbar_flutter/rollbar.dart' as rollbar;
 /// Example Flutter application using rollbar-flutter.
 Future<void> main() async {
   const config = rollbar.Config(
-      accessToken: '640cf35bbd6d433e82eeeb32c6789670',
+      accessToken: 'YOUR-ROLLBAR-ACCESSTOKEN',
       package: 'rollbar_flutter_example');
 
   await RollbarFlutter.run(config, () {
