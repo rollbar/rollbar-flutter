af0e10db513b584a1a4fc7d75fa60a59ad4ab9e1
Matias Pequeno <matias.pequeno@gmail.com>
2023-08-03 13:29:12 -0300

    Updated Dart SDK version and changelog

--- rollbar_dart/lib/src/notifier/notifier.dart
+++ rollbar_dart/lib/src/notifier/notifier.dart
@@ -6,7 +6,7 @@ import 'package:rollbar_dart/rollbar_dart.dart';
 @sealed
 abstract class Notifier {
   // notifier version to be updated with each new release: [todo] automate
-  static const version = '1.3.0';
+  static const version = '1.3.1';
   static const name = 'rollbar-dart';
 
   FutureOr<Context> notify(Context state, final Event event);
