c461e62a745a3e1a72ad7fe52ef1af624dff4c5f
Matias Pequeno <matias.pequeno@gmail.com>
2026-01-05 12:34:55 -0300

    Bump dart sdk version to 1.3.3, bump some other versions (#146)

--- rollbar_dart/lib/src/notifier/notifier.dart
+++ rollbar_dart/lib/src/notifier/notifier.dart
@@ -6,7 +6,7 @@ import 'package:rollbar_dart/rollbar_dart.dart';
 @sealed
 abstract class Notifier {
   // notifier version to be updated with each new release: [todo] automate
-  static const version = '1.3.2';
+  static const version = '1.3.3';
   static const name = 'rollbar-dart';
 
   FutureOr<Context> notify(Context state, final Event event);
